174b5ff611e78c545001454f8deb79c1e4b504ed
Khoi0codon <129353430+khoi-khong-co-don@users.noreply.github.com>
2023-08-13 12:09:48 +0700

    Update product.cpp

--- abstract factory/product.cpp
+++ abstract factory/product.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <string>
-
+// test git
 /**
  * Each distinct product of a product family should have a base interface. All
  * variants of the product must implement this interface.
